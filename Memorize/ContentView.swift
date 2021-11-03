4f7dece6114701289a5ddb6368fb2a487428d605
luochaoran <chaos1019@icloud.com>
2021-11-03 12:27:00 +0800

    Lecturer 01
    Update ContentView.swift

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3.0)
+            Text("Hello, world!")
         }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().preferredColorScheme(.dark)
+        ContentView()
     }
 }
