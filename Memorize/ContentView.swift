2dbd902bdfee8ad5cbfb30ebbe9c393be58b5115
luochaoran <chaos1019@icloud.com>
2021-11-03 09:45:12 +0800

    Initial project

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 骆超然 on 2021/11/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().preferredColorScheme(.dark)
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 骆超然 on 2021/11/1.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
